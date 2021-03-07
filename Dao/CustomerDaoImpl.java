693a525e16abd771b6a5368eecfd94d1bee28e4d
pi <payam.farah@live.com>
2021-03-07 04:17:34 -0800

    added queries to CustomerDaoImpl

--- Dao/CustomerDaoImpl.java
+++ Dao/CustomerDaoImpl.java
@@ -4,6 +4,25 @@ import javafx.collections.ObservableList;
 import models.Customer;
 
 public class CustomerDaoImpl implements CustomerDao<Customer>{
+    private String custId ="";
+
+
+    private final String db = "WJ07tms.customers";
+    private final String commaSpace = ", ";
+
+
+    private final String querySelectAll = "select * from " + db;
+    private final String querySelectSpecific = "select * from " + db + " where CUSTOMER_ID=";
+    private final String queryAddCust = "insert into " + db +
+        "(CUSTOMER_ID, CUSTOMER_NAME, ADDRESS, POSTAL_CODE, PHONE, DIVISION_ID) values(";
+    private final String queryUpdateCust = "update " + db + "where CUSTOMER_ID=" + custId +
+            "set ";
+    private final String queryDelete = "delete from " + db + " where CUSTOMER_ID=";
+
+
+
+
+
 
     @Override
     public Customer getCustomers(Integer id) {
